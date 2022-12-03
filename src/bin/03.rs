02e40478b56e2a7d459f0a6ecc3cbba22f38eeac
John Kryspin <johnkryspin@gmail.com>
2022-12-03 05:45:35 -0500

    day 3 done!

---
+++ src/bin/03.rs
@@ -0,0 +1,101 @@
+use std::collections::HashSet;
+use std::str::Chars;
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let chars = input.lines().into_iter().map(|line|{
+        let half = line.len()/2;
+        let (backpack_1, backpack_2) = line.split_at(half);
+        let mut seen:HashSet<char> = HashSet::new();
+        backpack_1.chars().into_iter().for_each(|c|{
+            seen.insert(c);
+        });
+        let dupe = backpack_2.chars().into_iter().find(|c|{
+            seen.contains(c)
+        });
+
+        return dupe;
+    });
+    Some(chars.fold(0, |prev, c|{
+        let val = priority(c.unwrap());
+        prev + val
+    }))
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut lines = input.lines();
+    let mut sum =0;
+    while let Some(x)= lines.next(){
+
+        let val = priority(Backpacks{
+            one: x.chars(),
+            two: lines.next().unwrap().chars(),
+            three: lines.next().unwrap().chars(),
+        }.common_char());
+        sum+=val;
+    }
+
+    Some(sum)
+}
+
+struct Backpacks<'a>{
+    one: Chars <'a>,
+    two: Chars <'a>,
+    three: Chars <'a>
+}
+impl<'a> Backpacks<'a>{
+    fn common_char(&self)->char{
+        let mut seen:HashSet<char> = HashSet::new();
+        self.one.clone().for_each(|c|{
+            seen.insert(c);
+        });
+
+        let mut seen_2:HashSet<char> = HashSet::new();
+        self.two.clone().for_each(|c|{
+            seen_2.insert(c);
+        });
+
+        seen.clone().iter().for_each(|c|{
+            if !seen_2.contains(c){
+                seen.remove(c);
+            }
+        });
+
+        let dupe = self.three.clone().find(|c|{
+            seen.contains(c)
+        });
+        dupe.unwrap()
+    }
+}
+
+fn priority(c:char)->u32{
+    let mut val = c as u32;
+    if c.is_ascii_lowercase(){
+        val -= 96;
+    }else{
+        val -= 38;
+    }
+    val
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 3);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 3);
+        assert_eq!(part_one(&input), Some(157));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 3);
+        assert_eq!(part_two(&input), Some(70));
+    }
+}
