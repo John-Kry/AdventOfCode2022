e97f3b2e1db6ad3af1df02dbc27cfb8056004af3
John Kryspin <johnkryspin@gmail.com>
2022-12-06 15:16:37 -0500

    formatting

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,4 +1,4 @@
-use std::collections::{HashMap};
+use std::collections::HashMap;
 
 pub fn part_one(input: &str) -> Option<usize> {
     get_first_unique(input, 4)
@@ -8,53 +8,54 @@ pub fn part_two(input: &str) -> Option<usize> {
     get_first_unique(input, 14)
 }
 
-fn get_first_unique(input: &str, size:usize)->Option<usize>{
+fn get_first_unique(input: &str, size: usize) -> Option<usize> {
     let mut seen = Seen::new();
-    let mut left =0;
+    let mut left = 0;
     let mut right = 0;
     let chars = input.as_bytes();
-    while right < chars.len(){
+    while right < chars.len() {
         while seen.get(chars[right] as char) > &0_usize {
             seen.remove(chars[left] as char);
-            left+=1;
+            left += 1;
         }
         if (right - left) == size {
             return Some(right);
         }
         seen.add(chars[right] as char);
-        right+=1;
+        right += 1;
     }
     None
 }
 
-struct Seen{
-    map: HashMap<char, usize>
+struct Seen {
+    map: HashMap<char, usize>,
 }
-
 
 impl Seen {
-    fn new()->Self{
-       Self{
-           map: HashMap::new()
-       }
+    fn new() -> Self {
+        Self {
+            map: HashMap::new(),
+        }
     }
 
-    fn get(&self, c:char) -> &usize {
+    fn get(&self, c: char) -> &usize {
         self.map.get(&c).unwrap_or(&0_usize)
     }
-   fn add(&mut self, c:char){
-       if self.map.contains_key(&c) {
-           self.map.insert(c,self.map.get(&c).expect("Should exist!") +1);
-       }else{
-           self.map.insert(c, 1);
-       }
-   }
-    fn remove(&mut self, c:char){
-        self.map.insert(c,self.map.get(&c).expect("Should exist") -1);
+
+    fn add(&mut self, c: char) {
+        if self.map.contains_key(&c) {
+            self.map
+                .insert(c, self.map.get(&c).expect("Should exist!") + 1);
+        } else {
+            self.map.insert(c, 1);
+        }
+    }
+
+    fn remove(&mut self, c: char) {
+        self.map
+            .insert(c, self.map.get(&c).expect("Should exist") - 1);
     }
 }
-
-
 
 fn main() {
     let input = &advent_of_code::read_file("inputs", 6);
