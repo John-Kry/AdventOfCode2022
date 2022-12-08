d7e60d621acea286afc0a0d7904bcad75b67e727
John Kryspin <johnkryspin@gmail.com>
2022-12-07 19:38:11 -0500

    done!

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,11 +1,51 @@
 use crate::Command::{CD, LS};
 use id_tree::InsertBehavior::AsRoot;
 use id_tree::*;
-use std::collections::HashSet;
+use std::borrow::BorrowMut;
 use std::str::FromStr;
 
 pub fn part_one(input: &str) -> Option<u32> {
     let mut tree: Tree<MyNode> = TreeBuilder::new().build();
+    generate_tree(tree.borrow_mut(), input);
+
+    Some(
+        tree.traverse_pre_order(tree.root_node_id().unwrap())
+            .unwrap()
+            .map(|n| total_size(&tree, n))
+            .filter(|n| n <= &100_000)
+            .sum::<u32>(),
+    )
+}
+fn total_size(tree: &Tree<MyNode>, node: &Node<MyNode>) -> u32 {
+    let mut total = node.data().files.iter().fold(0, |s, i| s + i.size);
+    for child in node.children() {
+        total += total_size(tree, tree.get(child).unwrap());
+    }
+    total
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut tree: Tree<MyNode> = TreeBuilder::new().build();
+
+    generate_tree(tree.borrow_mut(), input);
+
+    let total_space = 70000000_u32;
+    let used_space = total_size(&tree, tree.get(tree.root_node_id().unwrap()).unwrap());
+    let free_space = total_space - used_space;
+    let needed_free_space = 30000000_u32;
+    let minimum_space_to_free = needed_free_space.checked_sub(free_space).unwrap();
+
+    Some(
+        tree.traverse_pre_order(tree.root_node_id().unwrap())
+            .unwrap()
+            .map(|n| total_size(&tree, n))
+            .filter(|n| n >= &minimum_space_to_free)
+            .min()
+            .unwrap(),
+    )
+}
+
+fn generate_tree(tree: &mut Tree<MyNode>, input: &str) {
     let root_id: NodeId = tree
         .insert(
             Node::new(MyNode {
@@ -18,14 +58,14 @@ pub fn part_one(input: &str) -> Option<u32> {
 
     let mut curr_node = root_id.clone();
     let mut i = 0;
-    while i < input.lines().count() {
-        let mut line = input.lines().nth(i).unwrap();
+    let max = input.lines().count();
+    let lines = input.lines().into_iter().collect::<Vec<&str>>();
+    while i < max {
+        let mut line = lines.get(i).unwrap();
         let command: Command = line.parse().unwrap();
         match command {
             CD => {
                 let location = line.split(' ').nth(2).unwrap();
-                dbg!(location);
-                dbg!(tree.get(&curr_node).unwrap().data());
                 if location == "/" {
                     curr_node = root_id.clone();
                 } else if location == ".." {
@@ -43,12 +83,10 @@ pub fn part_one(input: &str) -> Option<u32> {
             }
             LS => {
                 i += 1;
-                line = input.lines().nth(i).unwrap();
+                line = lines.get(i).unwrap();
 
                 loop {
                     let (size_or_dir, name) = line.split_once(' ').unwrap();
-                    dbg!(size_or_dir);
-                    dbg!(name);
                     match size_or_dir {
                         "dir" => {
                             tree.insert(
@@ -60,151 +98,28 @@ pub fn part_one(input: &str) -> Option<u32> {
                             )
                             .expect("TODO: panic message");
                         }
-                        other => tree
+                        size => tree
                             .get_mut(&curr_node)
                             .unwrap()
                             .data_mut()
                             .files
                             .push(File {
-                                size: other.parse().unwrap(),
+                                size: size.parse().unwrap(),
                                 name: name.to_string(),
                             }),
                     }
                     i += 1;
-                    if i == input.lines().count() || input.lines().nth(i).unwrap().contains('$'){
-                        i-=1;
+                    if i == lines.len() || lines.get(i).unwrap().contains('$') {
+                        i -= 1;
                         break;
                     }
-                    line = input.lines().nth(i).unwrap();
+                    line = lines.get(i).unwrap();
                 }
             }
         }
         i += 1;
     }
-    let mut s = String::new();
-    &tree.write_formatted(&mut s);
-    println!("{}",s);
-
-    Some(
-        tree.traverse_pre_order(tree.root_node_id().unwrap())
-            .unwrap()
-            .inspect(|s|{
-                dbg!(s.data());
-            })
-            .map(|n| total_size(&tree, n))
-            .filter(|n|n <=&100_000)
-            .inspect(|s| {
-                dbg!(s);
-            })
-            .map(|i|i as u32)
-            .sum::<u32>(),
-    )
 }
-fn total_size(tree: &Tree<MyNode>, node: &Node<MyNode>) -> u32{
-    let mut total = node.data().files.iter().fold(0, |s, i| s + i.size);
-    for child in node.children() {
-        total += total_size(tree, tree.get(child).unwrap());
-    }
-    total
-}
-
-pub fn part_two(input: &str) -> Option<u32> {
-    let mut tree: Tree<MyNode> = TreeBuilder::new().build();
-    let root_id: NodeId = tree
-        .insert(
-            Node::new(MyNode {
-                label: "/".to_string(),
-                files: vec![],
-            }),
-            AsRoot,
-        )
-        .unwrap();
-
-    let mut curr_node = root_id.clone();
-    let mut i = 0;
-    while i < input.lines().count() {
-        let mut line = input.lines().nth(i).unwrap();
-        let command: Command = line.parse().unwrap();
-        match command {
-            CD => {
-                let location = line.split(' ').nth(2).unwrap();
-                dbg!(location);
-                dbg!(tree.get(&curr_node).unwrap().data());
-                if location == "/" {
-                    curr_node = root_id.clone();
-                } else if location == ".." {
-                    curr_node = tree.get(&curr_node).unwrap().parent().unwrap().clone();
-                } else {
-                    curr_node = tree
-                        .get(&curr_node)
-                        .unwrap()
-                        .children()
-                        .iter()
-                        .find(|x| tree.get(x).unwrap().data().label == location)
-                        .unwrap()
-                        .clone();
-                }
-            }
-            LS => {
-                i += 1;
-                line = input.lines().nth(i).unwrap();
-
-                loop {
-                    let (size_or_dir, name) = line.split_once(' ').unwrap();
-                    dbg!(size_or_dir);
-                    dbg!(name);
-                    match size_or_dir {
-                        "dir" => {
-                            tree.insert(
-                                Node::new(MyNode {
-                                    label: name.to_string(),
-                                    files: vec![],
-                                }),
-                                InsertBehavior::UnderNode(&curr_node),
-                            )
-                                .expect("TODO: panic message");
-                        }
-                        other => tree
-                            .get_mut(&curr_node)
-                            .unwrap()
-                            .data_mut()
-                            .files
-                            .push(File {
-                                size: other.parse().unwrap(),
-                                name: name.to_string(),
-                            }),
-                    }
-                    i += 1;
-                    if i == input.lines().count() || input.lines().nth(i).unwrap().contains('$'){
-                        i-=1;
-                        break;
-                    }
-                    line = input.lines().nth(i).unwrap();
-                }
-            }
-        }
-        i += 1;
-    }
-    let mut s = String::new();
-    &tree.write_formatted(&mut s);
-    println!("{}",s);
-
-    let total_space = 70000000_u32;
-    let used_space = total_size(&tree, tree.get(tree.root_node_id().unwrap()).unwrap());
-    let free_space = total_space.checked_sub(used_space).unwrap();
-    let needed_free_space = 30000000_u32;
-    let minimum_space_to_free = needed_free_space.checked_sub(free_space).unwrap();
-
-    Some(
-        tree.traverse_pre_order(tree.root_node_id().unwrap())
-            .unwrap()
-            .map(|n| total_size(&tree, n))
-            .filter(|n|n >=&minimum_space_to_free)
-            .min().unwrap()
-    )
-}
-
-fn generate_tree()
 
 #[derive(Debug)]
 enum Command {
@@ -222,7 +137,6 @@ impl FromStr for Command {
         if s.contains("ls") {
             return Ok(LS);
         }
-        dbg!(s);
         Err(())
     }
 }
