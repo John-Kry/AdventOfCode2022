abad6aecaeab96bbf7f6e6e90853beda4eb136f2
John Kryspin <johnkryspin@gmail.com>
2022-12-12 13:20:17 -0500

    cleaner

--- src/bin/12.rs
+++ src/bin/12.rs
@@ -1,6 +1,5 @@
-use std::cmp;
 use std::collections::VecDeque;
-use crate::Part::One;
+use crate::Part::{One, Two};
 
 pub fn part_one(input: &str) -> Option<u32> {
     let mut grid = Grid::new(input);
@@ -15,91 +14,20 @@ pub fn part_one(input: &str) -> Option<u32> {
     data.1 = true;
     queue.push_back((grid.start, 0));
 
-    shortest_path(grid, queue, One)
+    grid.shortest_path(queue, One)
 }
 
-fn shortest_path_2(mut grid: Grid, mut queue: VecDeque<(Pos, u32)>) -> Option<u32> {
-    let mut vex: Vec<(char, char)> = Vec::new();
 
 
-    while queue.len() > 0 {
-        let (v, count) = queue.pop_front().unwrap();
-        let curr = grid.get(v.x, v.y).unwrap();
-        if curr.0 == ('a' as u32) {
-            return Some(count);
+fn is_valid(part:Part, curr:(u32,bool), next:(u32,bool) )->bool{
+    return match part {
+        One => {
+            (next.0 as i32 - curr.0 as i32) <= 1
         }
-        // left
-        let left = grid.new_pos(v.x as i32 - 1, v.y as i32);
-        let right = grid.new_pos(v.x as i32 + 1, v.y as i32);
-        let up = grid.new_pos(v.x as i32, v.y as i32 + 1);
-        let down = grid.new_pos(v.x as i32, v.y as i32 - 1);
-
-        let mut dirs: Vec<Option<Pos>> = Vec::new();
-        dirs.push(left);
-        dirs.push(right);
-        dirs.push(up);
-        dirs.push(down);
-
-        dirs.iter().for_each(|a| {
-            if let Some(d) = a {
-                if let Some(next) = grid.get(d.x, d.y) {
-                    if next.1 == false && (( curr.0 as i32 - next.0 as i32) <= 1) {
-                        grid.set_visited(d.x, d.y);
-                        vex.push((char::from_u32(curr.0).unwrap(), char::from_u32(next.0).unwrap()));
-                        queue.push_back((d.clone(), count + 1));
-                    }
-                }
-            }
-        })
-    }
-   None
-}
-
-fn shortest_path(mut grid: Grid, mut queue: VecDeque<(Pos, u32)>, part: Part) -> Option<u32> {
-    let mut vex: Vec<(char, char)> = Vec::new();
-
-    while queue.len() > 0 {
-        let (v, count) = queue.pop_front().unwrap();
-        let curr = grid.get(v.x, v.y).unwrap();
-
-        match part {
-            One => {
-                if v == grid.end {
-                    return Some(count);
-                }
-            }
-            Part::Two => {
-                if curr.0 == ('a' as u32) {
-                   return Some(count);
-                }
-            }
+        Two => {
+            (curr.0 as i32 - next.0 as i32) <= 1
         }
-
-        // left
-        let left = grid.new_pos(v.x as i32 - 1, v.y as i32);
-        let right = grid.new_pos(v.x as i32 + 1, v.y as i32);
-        let up = grid.new_pos(v.x as i32, v.y as i32 + 1);
-        let down = grid.new_pos(v.x as i32, v.y as i32 - 1);
-
-        let mut dirs: Vec<Option<Pos>> = Vec::new();
-        dirs.push(left);
-        dirs.push(right);
-        dirs.push(up);
-        dirs.push(down);
-
-        dirs.iter().for_each(|a| {
-            if let Some(d) = a {
-                if let Some(next) = grid.get(d.x, d.y) {
-                    if next.1 == false && ((next.0 as i32 - curr.0 as i32) <= 1) {
-                        grid.set_visited(d.x, d.y);
-                        vex.push((char::from_u32(curr.0).unwrap(), char::from_u32(next.0).unwrap()));
-                        queue.push_back((d.clone(), count + 1));
-                    }
-                }
-            }
-        })
     }
-    None
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -116,9 +44,10 @@ pub fn part_two(input: &str) -> Option<u32> {
     data.1 = true;
 
     queue.push_back((grid.start, 0));
-    shortest_path_2(grid, queue)
+    grid.shortest_path(queue, Two)
 }
 
+#[derive(Clone, Copy)]
 enum Part{
     One,
     Two
@@ -147,12 +76,51 @@ struct Pos {
 impl Pos {}
 
 impl Grid {
-    fn clear_visited(&mut self){
-        self.data.iter_mut().for_each(|l|{
-            l.iter_mut().for_each(|x|{
-                x.1 = false;
+
+    fn shortest_path(&mut self, mut queue: VecDeque<(Pos, u32)>, part: Part) -> Option<u32> {
+
+        while queue.len() > 0 {
+            let (v, count) = queue.pop_front().unwrap();
+            let curr = self.get(v.x, v.y).unwrap();
+
+            match part {
+                One => {
+                    if v == self.end {
+                        return Some(count);
+                    }
+                }
+                Two => {
+                    if curr.0 == ('a' as u32) {
+                        return Some(count);
+                    }
+                }
+            }
+
+            // left
+            let left = self.new_pos(v.x as i32 - 1, v.y as i32);
+            let right = self.new_pos(v.x as i32 + 1, v.y as i32);
+            let up = self.new_pos(v.x as i32, v.y as i32 + 1);
+            let down = self.new_pos(v.x as i32, v.y as i32 - 1);
+
+            let mut dirs: Vec<Option<Pos>> = Vec::new();
+            dirs.push(left);
+            dirs.push(right);
+            dirs.push(up);
+            dirs.push(down);
+
+            dirs.iter().for_each(|a| {
+                if let Some(d) = a {
+                    if let Some(next) = self.get(d.x, d.y) {
+                        if next.1 == false && is_valid(part, curr,next)
+                        {
+                            self.set_visited(d.x, d.y);
+                            queue.push_back((d.clone(), count + 1));
+                        }
+                    }
+                }
             })
-        })
+        }
+        None
     }
     fn new_pos(&self, x: i32, y: i32) -> Option<Pos> {
         if x < 0 || y < 0 || x as usize >= self.x_len || y as usize >= self.y_len {
@@ -184,14 +152,14 @@ impl Grid {
             data.push(vec![]);
             let curr = data.get_mut(curr_x).unwrap();
             lines.clone().for_each(|line| {
-                let c = (line.as_bytes().get(curr_x).unwrap().clone() as char);
+                let c = line.as_bytes().get(curr_x).unwrap().clone() as char;
                 if c == 'S' {
                     start.x = curr_x;
-                    start.y = (curr.len());
+                    start.y = curr.len();
                     curr.push(('a' as u32, true))
                 } else if c == 'E' {
                     end.x = curr_x;
-                    end.y = (curr.len());
+                    end.y = curr.len();
                     curr.push(('z' as u32, false))
                 }else{
                     curr.push((c as u32, false))
